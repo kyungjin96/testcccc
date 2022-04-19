97b1c8a86c077adce61911ba22242652d3ce1417
kyungjin96 <wjsrudwls96@naver.com>
2022-04-19 17:36:54 +0900

    commit 2

---
+++ const참조2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int RefRetFunc2( int& ref)
+{
+    ref++;
+    return ref;
+}
+
+int main(void)
+{
+    int num1 = 1;
+    int num2 = RefRetFunc2(num1);
+
+
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+
+    num1 += 2;
+    num2 += 100;
+
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+
+    return 0;
+}
